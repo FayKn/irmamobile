7af694823eb2c4104988ace622ee98a636779266
Fay <61050421+FayKn@users.noreply.github.com>
2025-10-20 16:55:20 +0200

    added QR code scan test

--- integration_test/mfa_test.dart
+++ integration_test/mfa_test.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:irmamobile/src/models/enrollment_status.dart';
+import 'package:irmamobile/src/models/session.dart';
+import 'package:irmamobile/src/screens/scanner/scanner_screen.dart';
 import 'package:irmamobile/src/widgets/yivi_themed_button.dart';
+import 'package:irmamobile/src/screens/mfa/mfa_tab.dart';
 
 import 'helpers/helpers.dart';
 import 'irma_binding.dart';
+import 'qr_on_pin_screen_test.dart';
 import 'util.dart';
 
 main() {
@@ -30,7 +33,33 @@ main() {
     setUp(() => irmaBinding.setUp(experimentalFeatures: true));
     tearDown(() => irmaBinding.tearDown());
 
-    testWidgets('add mfa item', (WidgetTester tester) async {
+    testWidgets('Add MFA item via QR on homescreen', (WidgetTester tester) async {
+      await pumpAndUnlockApp(tester, irmaBinding.repository);
+      await tester.tapAndSettle(find.byKey(const Key('nav_button_scanner')));
+      // pretend to scan MFA QR code by creating a pointer directly and running the handler
+      Pointer pointer = MFAPointer(
+        inputUrl: 'otpauth://totp/TestIssuer:TestUser?issuer=TestIssuer&secret=LMSCQYMSVVQYSIUM&algorithm=SHA1&digits=6&period=30',
+      );
+
+      final ScannerScreenState scannerState = tester.state(find.byType(ScannerScreen));
+      scannerState.onQrScanned(pointer);
+      await tester.pump(); // Pump once to process the scan
+
+      // Navigate to MFA tab to verify the added item
+      await tester.tap(find.byKey(const Key('nav_button_mfa')));
+      await tester.pump();
+      // Pause the timer to prevent hanging on pumpAndSettle
+      final MfaTabState mfaState = tester.state(find.byType(MfaTab));
+      mfaState.timerPaused = true;
+      await tester.pumpAndSettle();
+
+      // verify item is added, the correctness of data is tested in unit tests within Go
+      expect(find.text('TestIssuer'), findsOneWidget);
+      expect(find.text('TestUser'), findsOneWidget);
+    });
+
+
+    testWidgets('add mfa item manually', (WidgetTester tester) async {
       await initAndNavToMfaScreen(tester);
       expect(find.byKey(const Key('add_mfa_manual_button')), findsOneWidget);
 
